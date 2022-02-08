b1445b46af3b7f16bb1f896c05ad4f05bf07f1ad
jarrodback <jb@jarrodback.co.uk>
2022-02-08 11:50:17 +0000

    Develop - Server setup

---
+++ server/app.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const createError = require("http-errors");
+const cookieSession = require("cookie-session");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+require("dotenv").config();
+
+var app = express();
+
+app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+
+app.use(
+    cookieSession({
+        name: "attendanceSystem-token",
+        secret: process.env.TOKEN_SECRET,
+        httpOnly: true,
+        //secure: true - Disabled as don't use HTTPS over local host so the cookie won't set.
+        keys: [process.env.TOKEN_SECRET],
+    })
+);
+
+/**
+ * OpenAPI (Swagger) Setup
+ */
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "ReadBooks: Online API",
+            version: "1.0.0",
+            description: "Documentation for the Attendance System API.",
+            contact: {
+                name: "Attendance System API Admin",
+                url: "http://localhost:8080",
+            },
+        },
+
+        servers: [
+            {
+                url: "http://localhost:3050",
+                description: "API Documentation",
+            },
+        ],
+    },
+    apis: ["./Routes/*.js"],
+
+    components: {
+        securitySchemes: {
+            cookieAuth: {
+                type: "apiKey",
+                in: "cookie",
+                name: "attendanceSystem-token",
+            },
+        },
+    },
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(specs, { explorer: true })
+);
+
+/**
+ * Router setup
+ */
+
+/**
+ * View Engine setup
+ */
+app.set("views", path.join(__dirname, "views"));
+app.use(logger("dev"));
+app.set("trust proxy", 1);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Configuring the main routes
+
+/**
+ * Error handling
+ */
+// Catch a 404 error
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+    // Only show errors in development build
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
