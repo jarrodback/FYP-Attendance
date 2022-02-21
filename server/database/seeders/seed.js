97b27ea28ede756c2d64d0d27b615a433b2229b0
jarrodback <jb@jarrodback.co.uk>
2022-02-21 19:36:15 +0000

    ATS-2 Try and fix workflow

--- server/database/seeders/seed.js
+++ server/database/seeders/seed.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV ?? "testing";
 const config = require("../../database/config")[env];
 const db = {};
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV ?? "testing";
 const config = require("../database/config")[env];
 
 // Create connection string from config.
