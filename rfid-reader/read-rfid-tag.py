7104a84cffda6e58ec83dbffd7669a602da18cf1
jarrodback <jb@jarrodback.co.uk>
2022-03-11 17:02:45 +0000

    ATS-8 Add env to python

--- rfid-reader/read-rfid-tag.py
+++ rfid-reader/read-rfid-tag.py
@@ -5,7 +5,13 @@ import RPi.GPIO as GPIO
 import requests
 import signal
 import datetime
+import time
+import os
 from mfrc522 import SimpleMFRC522
+from dotenv import load_dotenv
+
+# Import environment varaibles.
+load_dotenv()
 
 reader = SimpleMFRC522()
 
@@ -36,10 +42,11 @@ while continue_reading:
       print("Sending request to server...")
 
       # Send a request to the server with the value from the tag and the current time.
-      data = {'UserID': text, 'arrivalTime': datetime.datetime.now()}  
-      response = requests.post("http://192.168.1.33:3050/api/v1/attendance/reader", data=data)
+      data = {'UserId': text.rstrip(), 'arrivalTime': datetime.datetime.now()}  
+      server_addr = "http://" + os.getenv('SERVER_ADDR') + "/api/v1/attendance/reader"
+      response = requests.post(server_addr, data=data)
       print (response)
-      
-  # If unable to read tag, throw exception. 
-  except:
-      print ("An exception occured while reading for tags.")
+
+  # If unable to read tag, throw exception and print the error. 
+  except Exception as error:
+      print ("An exception occured while reading for tags.", error)
